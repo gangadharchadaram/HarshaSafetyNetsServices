e17ff707da30ac406e2b83ad4362d57af015586c
gangadharchadaram <saigangadhar06@gmail.com>
2025-06-17 08:24:29 +0530

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -63,7 +63,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-gray-400">
-            © 2024 Harsha Safety Nets Services. All rights reserved. | Professional Anti-Bird Control Services
+            © 2025 Harsha Safety Nets Services. All rights reserved. | Professional Anti-Bird Control Services
           </p>
         </div>
       </div>
